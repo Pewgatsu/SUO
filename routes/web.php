9b6782c9b9654c2ff34afdc0de9d4d67dc196be9
Pewgatsu <Jmsssss.qwerty15@gmail.com>
2021-08-26 00:19:40 +0800

    Set the login page as the landing page

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Auth::routes();
 
+
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/test',function(){
-    return view('test');
+Route::get('/login',[App\Http\Controllers\LoginController::class,'index'])->name('login');
+
+Route::get('registration',function(){
+    return view('registration');
 });
+
 
 Route::get('/dashboard', function (){
    return view('dashboard');
 });
+
+
