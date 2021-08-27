1380e24fd087c20b8f0659889767ce194ff65435
Rajan Elio <57312970+r-elio@users.noreply.github.com>
2021-08-27 19:01:18 +0800

    separate the dashboard front end

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\ComponentsController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UsersController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,10 +31,9 @@ Route::get('/register',[App\Http\Controllers\AuthController::class,'registerPage
 Route::post('/register',[App\Http\Controllers\AuthController::class,'registerUser']);
 Route::post('/login',[\App\Http\Controllers\AuthController::class,'login'])->name('login');
 
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/dashboard', function(){
-    return view('dashboard');
-});
+Route::get('/users', [UsersController::class, 'index'])->name('users');
 
 Route::get('/register2', function (){
     return view('auth.register');
