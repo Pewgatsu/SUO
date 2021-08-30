7320e2c7f22c9a08b006f105d80614b81ccc3a53
Pewgatsu <Jmsssss.qwerty15@gmail.com>
2021-08-30 14:20:53 +0800

    Added scripts

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.app');
 
+@push('scripts')
+    <script src="{{asset('js/validate_login.js')}}" defer></script>
+@endpush
+
 @section('content')
+
     <div class="container col-xl-10 col-xxl-8 px-4 py-5">
         <div class="row align-items-center g-lg-5 py-5">
-
-            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-                <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                </symbol>
-            </svg>
 
             <div class="col-md-10 mx-auto col-lg-5">
 
@@ -20,22 +19,32 @@
                 @endif
 
 
-                <form  action="{{route('login')}}" class="needs-validation p-4 p-md-5 border rounded-3 bg-light"
+                <form id="loginForm" action="{{route('login')}}" class="needs-validation p-4 p-md-5 border rounded-3 bg-light"
                       method="POST" >
                     @csrf
 
                     <div class="form-floating flex-fill mb-4 ">
-                        <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" value="{{old('username')}}" placeholder="#" autocomplete="username" autofocus>
+                        <input type="text" id="usernameField" name="username" class="form-control @error('username') is-invalid @enderror" placeholder="#"  autofocus>
                         <label for="usernameField">Username</label>
+                        <div class="valid-tooltip position-relative">Looks Good!</div>
                         @error('username')
+                        <script>
+                            let username = document.getElementById('usernameField');
+                            username.addEventListener("blur", validateUsername);
+                        </script>
                         <div class="invalid-tooltip position-relative">Field must not be empty!</div>
                         @enderror
                     </div>
 
                     <div class="form-floating flex-fill mb-4 ">
-                        <input type="text" id="passwordField" name="password" class="form-control @error('password') is-invalid @enderror" value="{{old('password')}}" placeholder="#">
+                        <input type="text" id="passwordField" name="password" class="form-control @error('password') is-invalid @enderror"placeholder="#" autofocus>
                         <label for="usernameField">Password</label>
+                        <div class="valid-tooltip position-relative">Looks Good!</div>
                         @error('password')
+                        <script>
+                            let password = document.getElementById('passwordField');
+                            password.addEventListener("blur", validatePassword);
+                        </script>
                         <div class="invalid-tooltip position-relative">Field must not be empty!</div>
                         @enderror
                     </div>
