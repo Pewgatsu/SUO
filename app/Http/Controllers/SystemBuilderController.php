0c587013ad910d2cb7a4234a2ff8857ac6960510
Sherry Gil Egana <sherrrygil.egana@gmail.com>
2021-09-05 00:26:56 +0800

    Disabled buttons and checkbox if component button is not pressed
    Used session variables to disable the checkbox and order button if the component button is not pressed

--- app/Http/Controllers/SystemBuilderController.php
+++ app/Http/Controllers/SystemBuilderController.php
@@ -36,52 +36,129 @@ class SystemBuilderController extends Controller
 
                 switch ($key){
                     case "motherboards":
+
+                        //Fetch the name and component id in the database using eloquent and query builder
                         foreach (Models\Motherboard::all() as $hold){
                             $id = DB::table('components')->find($hold->component_id);
                             array_push($holder, array('id' =>$id->id, 'name'=>$id->name));
                         }
+                        //checks if session motherboards is created? TRUE overrides the content : FALSE creates a session
+                        if ($request->session()->has('motherboards')) {
+                            session(['motherboards'=> 'pressed motherboard again','motherboardsPrice'=>'10']);
+                        }else{
+                            session(['motherboards'=> 'pressed motherboard','motherboardsPrice'=>'00','motherboardsCheckBox'=>' ','motherboardsOrder'=>' ']);
+                        }
+
                         break;
+
                     case "cpus":
+
+                        //Fetch the name and component id in the database using eloquent and query builder
                         foreach ( Models\CPU::all() as $hold){
                             $id = DB::table('components')->find($hold->component_id);
                             array_push($holder, array('id' =>$id->id, 'name'=>$id->name));
                         }
+                        //checks if session cpus is created? TRUE overrides the content : FALSE creates a session
+                        if ($request->session()->has('cpus')) {
+                            session(['cpus'=> 'pressed cpus again','cpusPrice'=>'10']);
+                        }else{
+                            session(['cpus'=> 'pressed cpus','cpusPrice'=>'00','cpusCheckBox'=>' ','cpusOrder'=>' ']);
+                        }
+
                         break;
+
                     case "cpu_coolers":
+
+                        //Fetch the name and component id in the database using eloquent and query builder
                         foreach ( Models\CPUCooler::all() as $hold){
                             $id = DB::table('components')->find($hold->component_id);
                             array_push($holder, array('id' =>$id->id, 'name'=>$id->name));
                         }
+                        //checks if session cpu_coolers is created? TRUE overrides the content : FALSE creates a session
+                        if ($request->session()->has('cpu_coolers')) {
+                            session(['cpu_coolers'=> 'pressed cpu_coolers again','cpu_coolersPrice'=>'10']);
+                        }else{
+                            session(['cpu_coolers'=> 'pressed cpu_coolers','cpu_coolersPrice'=>'00','cpu_coolersCheckBox'=>' ','cpu_coolersOrder'=>' ']);
+                        }
+
                         break;
                     case "graphics_cards":
+
+                        //Fetch the name and component id in the database using eloquent and query builder
                         foreach (Models\GraphicsCard::all() as $hold){
                             $id = DB::table('components')->find($hold->component_id);
                             array_push($holder, array('id' =>$id->id, 'name'=>$id->name));
                         }
+                        //checks if session graphics_cards is created? TRUE overrides the content : FALSE creates a session
+                        if ($request->session()->has('graphics_cards')) {
+                            session(['graphics_cards'=> 'pressed graphics_cards again','graphics_cardsPrice'=>'10']);
+                        }else{
+                            session(['graphics_cards'=> 'pressed graphics_cards','graphics_cardsPrice'=>'00','graphics_cardsCheckBox'=>' ','graphics_cardsOrder'=>' ']);
+                        }
+
                         break;
+
                     case "rams":
+
+                        //Fetch the name and component id in the database using eloquent and query builder
                         foreach (Models\RAM::all() as $hold){
                             $id = DB::table('components')->find($hold->component_id);
                             array_push($holder, array('id' =>$id->id, 'name'=>$id->name));
                         }
+                        //checks if session rams is created? TRUE overrides the content : FALSE creates a session
+                        if ($request->session()->has('rams')) {
+                            session(['rams'=> 'pressed rams again','ramsPrice'=>'10']);
+                        }else{
+                            session(['rams'=> 'pressed rams','ramsPrice'=>'00','ramsCheckBox'=>' ','ramsOrder'=>' ']);
+                        }
+
                         break;
+
                     case "storages":
+
+                        //Fetch the name and component id in the database using eloquent and query builder
                         foreach (Models\Storage::all() as $hold){
                             $id = DB::table('components')->find($hold->component_id);
                             array_push($holder, array('id' =>$id->id, 'name'=>$id->name));
                         }
+                        //checks if session storages is created? TRUE overrides the content : FALSE creates a session
+                        if ($request->session()->has('storages')) {
+                            session(['storages'=> 'pressed storages again','storagesPrice'=>'10']);
+                        }else{
+                            session(['storages'=> 'pressed storages','storagesPrice'=>'00','storagesCheckBox'=>' ','storagesOrder'=>' ']);
+                        }
+
                         break;
+
                     case "psus":
+
+                        //Fetch the name and component id in the database using eloquent and query builder
                         foreach (Models\PSU::all() as $hold){
                             $id = DB::table('components')->find($hold->component_id);
                             array_push($holder, array('id' =>$id->id, 'name'=>$id->name));
                         }
+                        //checks if session psus is created? TRUE overrides the content : FALSE creates a session
+                        if ($request->session()->has('psus')) {
+                            session(['psus'=> 'pressed psus again','psusPrice'=>'10']);
+                        }else{
+                            session(['psus'=> 'pressed psus','psusPrice'=>'00','psusCheckBox'=>' ','psusOrder'=>' ']);
+                        }
+
                         break;
                     case "computer_cases":
+
+                        //Fetch the name and component id in the database using eloquent and query builder
                         foreach (Models\ComputerCase::all() as $hold){
                             $id = DB::table('components')->find($hold->component_id);
                             array_push($holder, array('id' =>$id->id, 'name'=>$id->name));
                         }
+                        //checks if session computer_cases is created? TRUE overrides the content : FALSE creates a session
+                        if ($request->session()->has('computer_cases')) {
+                            session(['computer_cases'=> 'pressed computer_cases again','computer_casesPrice'=>'10']);
+                        }else{
+                            session(['computer_cases'=> 'pressed computer_cases','computer_casesPrice'=>'00','computer_casesCheckBox'=>' ','computer_casesOrder'=>' ']);
+                        }
+
                         break;
                 }
             }
--- resources/views/systemBuilder/systemBuilder.blade.php
+++ resources/views/systemBuilder/systemBuilder.blade.php
@@ -20,16 +20,16 @@
                     <form action="/components" method="post">
                         <input type="hidden" name="selectedComponents" value="motherboards">
                         {{csrf_field()}}
-                        <input type="submit" name="selectedComponent" value="{{'+'}}" class="btn btn-info col-12">
+                        <input type="submit" name="selectedComponent" value="{{session('motherboards','+')}}" class="btn btn-info col-12">
                     </form>
                 </td>
-                <td class="text-center">PRICE</td>
+                <td class="text-center">{{session('motherboardsPrice','--')}}</td>
                 <td class="text-center">
                     <form name="ownedComponent" method="post">
                         <div class="form-check">
                             <label class="form-check-label">
                                 <input type="hidden" name="ownedMotherboard" value="">
-                                <input type="checkbox" id="ownedComponentMotherboard" name="ownedComponentMotherboard" class="form-check-input">Owned
+                                <input type="checkbox" id="ownedComponentMotherboard" name="ownedComponentMotherboard" {{session('motherboardsCheckBox','disabled')}} class="form-check-input">Owned
                             </label>
                         </div>
                     </form>
@@ -37,7 +37,7 @@
                 <td>
                     <form method="post">
                         <input type="hidden" name="orderComponentMotherboard">
-                        <button type="submit" class="btn btn-info col-12" name="orderComponent">Order</button>
+                        <button type="submit" class="btn btn-info col-12" {{session('motherboardsOrder','disabled')}} name="orderComponent">Order</button>
                     </form>
                 </td>
             </tr>
@@ -48,16 +48,16 @@
                     <form action="/components" method="post">
                         <input type="hidden" name="selectedComponents" value="cpus">
                         {{csrf_field()}}
-                        <input type="submit" name="selectedComponent" value="+" class="btn btn-info col-12">
+                        <input type="submit" name="selectedComponent" value="{{session('cpus','+')}}" class="btn btn-info col-12">
                     </form>
                 </td>
-                <td class="text-center">PRICE</td>
+                <td class="text-center">{{session('cpusPrice','--')}}</td>
                 <td class="text-center">
                     <form name="ownedComponent" method="post">
                         <div class="form-check">
                             <label class="form-check-label">
                                 <input type="hidden" name="ownedCpu" value="">
-                                <input type="checkbox" id="ownedComponentCpu" name="ownedComponentCpu" class="form-check-input">Owned
+                                <input type="checkbox" id="ownedComponentCpu" name="ownedComponentCpu" {{session('cpusCheckBox','disabled')}} class="form-check-input">Owned
                             </label>
                         </div>
                     </form>
@@ -65,7 +65,7 @@
                 <td>
                     <form method="post">
                         <input type="hidden" name="orderComponentCpu">
-                        <button type="submit" class="btn btn-info col-12" name="orderComponent">Order</button>
+                        <button type="submit" class="btn btn-info col-12" {{session('cpusOrder','disabled')}} name="orderComponent">Order</button>
                     </form>
                 </td>
             </tr>
@@ -76,16 +76,16 @@
                     <form action="/components" method="post">
                         <input type="hidden" name="selectedComponents" value="cpu_coolers">
                         {{csrf_field()}}
-                        <input type="submit" name="selectedComponent" value="+" class="btn btn-info col-12">
+                        <input type="submit" name="selectedComponent" value="{{session('cpu_coolers','+')}}" class="btn btn-info col-12">
                     </form>
                 </td>
-                <td class="text-center">PRICE</td>
+                <td class="text-center">{{session('cpu_coolersPrice','--')}}</td>
                 <td class="text-center">
                     <form name="ownedComponent" method="post">
                         <div class="form-check">
                             <label class="form-check-label">
                                 <input type="hidden" name="ownedCpuCooler" value="">
-                                <input type="checkbox" id="ownedComponentCpuCooler" name="ownedComponentCpuCooler" class="form-check-input">Owned
+                                <input type="checkbox" id="ownedComponentCpuCooler" name="ownedComponentCpuCooler" {{session('cpu_coolersCheckBox','disabled')}} class="form-check-input">Owned
                             </label>
                         </div>
                     </form>
@@ -93,7 +93,7 @@
                 <td>
                     <form method="post">
                         <input type="hidden" name="orderComponentCpuCooler">
-                        <button type="submit" class="btn btn-info col-12" name="orderComponent">Order</button>
+                        <button type="submit" class="btn btn-info col-12" {{session('cpu_coolersOrder','disabled')}} name="orderComponent">Order</button>
                     </form>
                 </td>
             </tr>
@@ -104,16 +104,16 @@
                     <form action="/components" method="post">
                         <input type="hidden" name="selectedComponents" value="graphics_cards">
                         {{csrf_field()}}
-                        <input type="submit" name="selectedComponent" value="+" class="btn btn-info col-12">
+                        <input type="submit" name="selectedComponent" value="{{session('graphics_cards','+')}}" class="btn btn-info col-12">
                     </form>
                 </td>
-                <td class="text-center">PRICE</td>
+                <td class="text-center">{{session('graphics_cardsPrice','--')}}</td>
                 <td class="text-center">
                     <form name="ownedComponent" method="post">
                         <div class="form-check">
                             <label class="form-check-label">
                                 <input type="hidden" name="ownedGpu" value="">
-                                <input type="checkbox" id="ownedComponentGpu" name="ownedComponentGpu" class="form-check-input">Owned
+                                <input type="checkbox" id="ownedComponentGpu" name="ownedComponentGpu" {{session('graphics_cardsCheckBox','disabled')}} class="form-check-input">Owned
                             </label>
                         </div>
                     </form>
@@ -121,7 +121,7 @@
                 <td>
                     <form method="post">
                         <input type="hidden" name="orderComponentGpu">
-                        <button type="submit" class="btn btn-info col-12" name="orderComponent">Order</button>
+                        <button type="submit" class="btn btn-info col-12" {{session('graphics_cardsOrder','disabled')}} name="orderComponent">Order</button>
                     </form>
                 </td>
             </tr>
@@ -132,16 +132,16 @@
                     <form action="/components" method="post">
                         <input type="hidden" name="selectedComponents" value="rams">
                         {{csrf_field()}}
-                        <input type="submit" name="selectedComponent" value="+" class="btn btn-info col-12">
+                        <input type="submit" name="selectedComponent" value="{{session('rams','+')}}" class="btn btn-info col-12">
                     </form>
                 </td>
-                <td class="text-center">PRICE</td>
+                <td class="text-center">{{session('ramsPrice','--')}}</td>
                 <td class="text-center">
                     <form name="ownedComponent" method="post">
                         <div class="form-check">
                             <label class="form-check-label">
                                 <input type="hidden" name="ownedRam" value="">
-                                <input type="checkbox" id="ownedComponentRam" name="ownedComponentRam" class="form-check-input">Owned
+                                <input type="checkbox" id="ownedComponentRam" name="ownedComponentRam" {{session('ramsCheckBox','disabled')}} class="form-check-input">Owned
                             </label>
                         </div>
                     </form>
@@ -149,7 +149,7 @@
                 <td>
                     <form method="post">
                         <input type="hidden" name="orderComponentRam">
-                        <button type="submit" class="btn btn-info col-12" name="orderComponent">Order</button>
+                        <button type="submit" class="btn btn-info col-12" {{session('ramsOrder','disabled')}} name="orderComponent">Order</button>
                     </form>
                 </td>
             </tr>
@@ -160,16 +160,16 @@
                     <form action="/components" method="post">
                         <input type="hidden" name="selectedComponents" value="storages">
                         {{csrf_field()}}
-                        <input type="submit" name="selectedComponent" value="+" class="btn btn-info col-12">
+                        <input type="submit" name="selectedComponent" value="{{session('storages','+')}}" class="btn btn-info col-12">
                     </form>
                 </td>
-                <td class="text-center">PRICE</td>
+                <td class="text-center">{{session('storagesPrice','--')}}</td>
                 <td class="text-center">
                     <form name="ownedComponent" method="post">
                         <div class="form-check">
                             <label class="form-check-label">
                                 <input type="hidden" name="ownedStorage" value="">
-                                <input type="checkbox" id="ownedComponentStorage" name="ownedComponentStorage" class="form-check-input">Owned
+                                <input type="checkbox" id="ownedComponentStorage" name="ownedComponentStorage" {{session('storagesCheckBox','disabled')}} class="form-check-input">Owned
                             </label>
                         </div>
                     </form>
@@ -177,7 +177,7 @@
                 <td>
                     <form method="post">
                         <input type="hidden" name="orderComponentStorage">
-                        <button type="submit" class="btn btn-info col-12" name="orderComponent">Order</button>
+                        <button type="submit" class="btn btn-info col-12" {{session('storagesOrder','disabled')}} name="orderComponent">Order</button>
                     </form>
                 </td>
             </tr>
@@ -188,16 +188,16 @@
                     <form action="/components" method="post">
                         <input type="hidden" name="selectedComponents" value="psus">
                         {{csrf_field()}}
-                        <input type="submit" name="selectedComponent" value="+" class="btn btn-info col-12">
+                        <input type="submit" name="selectedComponent" value="{{session('psus','+')}}" class="btn btn-info col-12">
                     </form>
                 </td>
-                <td class="text-center">PRICE</td>
+                <td class="text-center">{{session('psusPrice','--')}}</td>
                 <td class="text-center">
                     <form name="ownedComponent" method="post">
                         <div class="form-check">
                             <label class="form-check-label">
                                 <input type="hidden" name="ownedPowerSupply" value="">
-                                <input type="checkbox" id="ownedComponentPowerSupply" name="ownedComponentPowerSupply" class="form-check-input">Owned
+                                <input type="checkbox" id="ownedComponentPowerSupply" name="ownedComponentPowerSupply" {{session('psusCheckBox','disabled')}} class="form-check-input">Owned
                             </label>
                         </div>
                     </form>
@@ -205,7 +205,7 @@
                 <td>
                     <form method="post">
                         <input type="hidden" name="orderComponentPowerSupply">
-                        <button type="submit" class="btn btn-info col-12" name="orderComponent">Order</button>
+                        <button type="submit" class="btn btn-info col-12" {{session('psusOrder','disabled')}} name="orderComponent">Order</button>
                     </form>
                 </td>
             </tr>
@@ -216,16 +216,16 @@
                 <form action="/components" method="post">
                     <input type="hidden" name="selectedComponents" value="computer_cases">
                     {{csrf_field()}}
-                    <input type="submit" name="selectedComponent" value="+" class="btn btn-info col-12">
+                    <input type="submit" name="selectedComponent" value="{{session('computer_cases','+')}}" class="btn btn-info col-12">
                 </form>
             </td>
-            <td class="text-center">PRICE</td>
+            <td class="text-center">{{session('computer_casesPrice','--')}}</td>
             <td class="text-center">
                 <form name="ownedComponent" method="post">
                     <div class="form-check">
                         <label class="form-check-label">
                             <input type="hidden" name="ownedComputerCase" value="">
-                            <input type="checkbox" id="ownedComponentComputerCase" name="ownedComponentComputerCase" class="form-check-input">Owned
+                            <input type="checkbox" id="ownedComponentComputerCase" name="ownedComponentComputerCase" {{session('computer_casesCheckBox','disabled')}} class="form-check-input">Owned
                         </label>
                     </div>
                 </form>
@@ -233,7 +233,7 @@
             <td>
                 <form method="post">
                     <input type="hidden" name="orderComponentComputerCase">
-                    <button type="submit" class="btn btn-info col-12" name="orderComponent">Order</button>
+                    <button type="submit" class="btn btn-info col-12" {{session('computer_casesOrder','disabled')}} name="orderComponent">Order</button>
                 </form>
             </td>
         </tr>
