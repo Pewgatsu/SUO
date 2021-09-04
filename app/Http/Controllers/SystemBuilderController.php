796353f8f721e627f3dbcab8270a6cb30e2c42a4
Pewgatsu <Jmsssss.qwerty15@gmail.com>
2021-09-04 17:34:06 +0800

    Refactored systemBuilder View to builder

--- app/Http/Controllers/SystemBuilderController.php
+++ app/Http/Controllers/SystemBuilderController.php
@@ -9,7 +9,7 @@ class SystemBuilderController extends Controller
 {
     //
     public function index(){
-        return view('systemBuilder/systemBuilder');
+        return view('systemBuilder.builder');
     }
 
 
