264a865a3a9c8dbdb992f2d183b1b942bef0a3bc
Pewgatsu <Jmsssss.qwerty15@gmail.com>
2021-09-04 15:58:57 +0800

    Revert "Init"
    This reverts commit db131132c94190a2b678e54f40136e7544ee6475.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,25 +13,23 @@ class AuthController extends Controller
 {
     //
     public function loginPage(){
-        return view('auth.login');
+        return view('login');
     }
 
     public function registerPage(){
-        return view('auth.login');
+        return view('registration');
     }
 
-    public function login(LoginRequest $request,$account){
+    public function login(LoginRequest $request){
 
         $credentials = $request->only('username','password');
         $account = Account::where('username',$request->username)->first();
 
-
-
-//        if(Auth::attempt($credentials) && $credentials->'account_type' == ){
-//            return redirect()->route('builder');
-//        }else{
-//            return redirect()->back()->with('error','Invalid Credentials');
-//        }
+        if(Auth::attempt($credentials)){
+            return view('dashboard');
+        }else{
+            return redirect()->back()->with('error','Invalid Credentials');
+        }
     }
 
     //validate the input in request
