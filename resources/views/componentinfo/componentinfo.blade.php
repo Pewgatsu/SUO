358cd459f3e5840f4cbffa2b15c69fb4633e2869
akreyes3 <agericoreyes59@gmail.com>
2021-09-13 12:29:58 +0800

    changes4 sept 13

--- resources/views/componentinfo/componentinfo.blade.php
+++ resources/views/componentinfo/componentinfo.blade.php
@@ -2,55 +2,42 @@
 
 @section('content')
 
-    <div class="p-5 card mx-auto" style="margin-top: 58px; width: 50rem; ">
-        <!-- The Grid -->
-        <div class="d-flex align-items-start bg-light mb-3">
-            <div id="card" class="card-header">
-                <h3 class="card-title">Product Info</h3> {{--how to display component info name here--}}
-                <div class="container">
-
-                </div>
-            </div>
-            <br>
-            <!-- left Column -->
-            <div class="align-content-lg-start">
-                <div class="row-cols-xxl-auto" >
-                    <div class="card">
-                        <div style="overflow-x:auto;">
-                            <img src="<?= ?>" alt="" width="100%" height="400" />
-                            <br><br>
-                            <table width="100%" >
-
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Right Column -->
-            <div class="align-content-lg-end">
-                <div class="row-cols-xxl-auto" >
-                    <div class="card">
-                        <div style="overflow-x:auto;">
-                            <table id="component_info" class="table table-striped table-hover" style="width:100%">
-                                <thead>
-                                <tr>
-                                    <th>Component Name</th> {{--how to display component info here--}}
-                                    <th>Component Name</th> {{--how to display component info here--}}
-                                    <th>Component Type</th> {{--how to display component info here--}}
-                                    <th>Price</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+    <div class="container" style="margin-top: 58px; width: 50rem;">
+        <div id="card" class="card-header">
+            <h3 class="card-title">Product Info</h3> {{--how to display component info name here--}}
+            <div class="container">
 
             </div>
         </div>
-    </div>
 
+        <!-- left Column -->
+        <div class="d-flex align-items-start bg-light mb-3">
+            <div class="card">
+                <div class= "card-body" style="overflow-x:auto;">
+                    <img src="" alt="" width="100%" height="400" />
+                </div>
+            </div>
+        </div>
+
+        <!-- Right Column -->
+        <div class="d-flex align-items-end bg-light mb-3">
+            <div class="card">
+                <div class= "card-body" style="overflow-x:auto;">
+                    <table id="component_info" class="table table-striped table-hover" style="width:100%">
+                        <thead>
+                        <tr>
+                            <th>Component Name</th> {{--how to display component info here--}}
+                            <th>Component Name</th> {{--how to display component info here--}}
+                            <th>Component Type</th> {{--how to display component info here--}}
+                            <th>Price</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,6 +11,17 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     @yield('head')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+
+    <script src="//cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 
