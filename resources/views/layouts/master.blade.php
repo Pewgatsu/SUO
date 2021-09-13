14493e223810d520f0e1d3ec39d5cc1e5c1735dd
akreyes3 <agericoreyes59@gmail.com>
2021-09-13 12:56:51 +0800

    changes6 sept 13

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -31,8 +31,17 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-
+    <script>
+    $(document).ready(function () {
+    $('#searchpagecomponents_table').DataTable({
+    "processing":true,
+    "serverSide":true,
+    });
+    });
+    $(document).ready(function () {
+    $('#searchpagestore_table').DataTable();
+    });
+    </script>
 </head>
 <body>
 <!-- navbar -->
