907630713ba5a5db233fa6c7e4922ac72173c14f
Rajan Elio <57312970+r-elio@users.noreply.github.com>
2021-09-24 21:44:37 +0800

    added delete profile pic functionality when deleting the account

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,7 +17,13 @@ class UsersController extends Controller
 
     public function remove(Account $account)
     {
+        // Delete Profile Pic
+        if (file_exists(public_path('images/accounts/' . $account->profile_path))){
+            unlink(public_path('images/accounts/' . $account->profile_path));
+        }
+
         $account->delete();
+
         return back();
     }
 
