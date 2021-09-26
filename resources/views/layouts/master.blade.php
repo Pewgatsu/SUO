28885c7421de4b2e0bae376cc2d03184c3a5396b
Pewgatsu <Jmsssss.qwerty15@gmail.com>
2021-09-26 13:32:12 +0800

    Re-added select2

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,11 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-{{--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"--}}
-{{--          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">--}}
-   {{-- @stack('scripts')--}}
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -18,9 +13,8 @@
     <link rel="stylesheet" href="//cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
     <script src="//cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 
-    <!-- Fonts -->
+
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 
@@ -43,7 +37,7 @@
 
 </head>
 <body>
-<!-- navbar -->
+
 
 <div id="app" >
     <main class="bg-light">
