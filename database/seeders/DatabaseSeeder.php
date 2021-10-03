78a37504036e5d5be40c2ee2a658889c3b9b06f3
Pewgatsu <Jmsssss.qwerty15@gmail.com>
2021-10-03 20:24:46 +0800

    Added seeder for three account types. Admin: username: admin, pw: password Customer: username: customer, pw: password Seller: username: seller, pw: password

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Account;
+use DateTime;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $date = new DateTime();
+        Account::insert([
+            'username' => 'admin',
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+            'account_type' => 'admin',
+            'is_admin' => true,
+            'firstname' => 'Test',
+            'lastname' => 'Admin',
+            'created_at' => $date,
+            'updated_at' => $date,
+        ]);
+
+        Account::insert([
+            'username' => 'seller',
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+            'account_type' => 'seller',
+            'is_admin' => true,
+            'firstname' => 'Test',
+            'lastname' => 'Seller',
+            'created_at' => $date,
+            'updated_at' => $date,
+        ]);
+
+        Account::insert([
+            'username' => 'customer',
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+            'account_type' => 'customer',
+            'is_admin' => true,
+            'firstname' => 'Test',
+            'lastname' => 'Customer',
+            'created_at' => $date,
+            'updated_at' => $date,
+        ]);
     }
 }
