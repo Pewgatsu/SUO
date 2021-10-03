54daf1ffdb3823e415d37f817c492dc588f140d3
Pewgatsu <Jmsssss.qwerty15@gmail.com>
2021-10-03 20:04:39 +0800

    Added Middleware to check account type

---
+++ app/Http/Middleware/IsCustomer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsCustomer
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->user()->account_type != 'customer'){
+            abort(403);
+        }
+        return $next($request);
+    }
+}
