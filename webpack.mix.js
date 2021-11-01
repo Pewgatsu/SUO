f7b69c0f9870b194b29d35166991511521463cbc
Pewgatsu <Jmsssss.qwerty15@gmail.com>
2021-11-01 11:34:00 +0800

    edit for production

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,11 +15,15 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
 
+
+
 mix.postCss('resources/css/login_nav.css','public/css');
 mix.postCss('resources/css/styles.css','public/css');
+mix.postCss('resources/css/toastr.css','public/css');
 
 mix.js('resources/js/validate_login.js','public/js');
 mix.js('resources/js/validate_register.js','public/js');
-mix.js('resources/js/scripts.js','public/js');
+// mix.js('resources/js/scripts.js','public/js');
+// mix.js('resources/js/toastr.js','public/js');
 
 
